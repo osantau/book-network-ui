47e75cbde0eddcaf2270d29b1921f3998ab5a0a9
osantau <octavian.santau@plastor.ro>
2026-05-13 16:07:54 +0300

    Show errors.

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthenticationRequest } from '../../services/models';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import {authenticate, Authenticate$Params} from '../../services/fn/authentication/authenticate';
+import { authenticate, Authenticate$Params } from '../../services/fn/authentication/authenticate';
 import { ApiConfiguration } from '../../services/api-configuration';
 import { HttpClient } from '@angular/common/http';
 
@@ -16,18 +16,25 @@ export class Login {
   authRequest: AuthenticationRequest = { email: '', password: '' };
   errorMsg: Array<string> = [];
 
-  constructor(private router: Router, private config: ApiConfiguration,private http: HttpClient) { }
+  constructor(private router: Router, private config: ApiConfiguration, private http: HttpClient) { }
 
   login() {
     this.errorMsg = [];
     const loginParams: Authenticate$Params = { body: this.authRequest };
     authenticate(this.http, this.config.rootUrl, loginParams).subscribe({
       next: (response) => {
-        console.log(response);
-  },error: (err) => {
+        //todo save the token
+        this.router.navigate(['books']);
+      }, error: (err) => {
         console.log(err);
-        this.errorMsg.push('Login failed. Please check your credentials and try again.');
-      }});
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push('Login failed. Please check your credentials and try again.');
+        }
+
+      }
+    });
   }
   register() {
     this.router.navigate(['register']);
