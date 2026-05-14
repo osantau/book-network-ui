943c52fc1d4de9f1ce27425b6f6cee5dff86c412
osantau <octavian.santau@plastor.ro>
2026-05-14 12:32:30 +0300

    Implement menu styling

--- src/app/modules/book/components/menu/menu.ts
+++ src/app/modules/book/components/menu/menu.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookRoutingModule } from "../../book-routing-module";
 
 @Component({
@@ -7,9 +7,26 @@ import { BookRoutingModule } from "../../book-routing-module";
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
-export class Menu {
-logout() {
-throw new Error('Method not implemented.');
-}
+export class Menu implements OnInit {
+
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(l => {
+      if (window.location.href.endsWith(l.getAttribute("href") || '')) {
+        l.classList.add('active');
+      }
+
+      l.addEventListener('click', () => {
+        linkColor.forEach(ln => ln.classList.remove('active'));
+        l.classList.add('active');
+      });
+    }
+    );
+
+  }
+
+  logout() {
+    throw new Error('Method not implemented.');
+  }
 
 }
